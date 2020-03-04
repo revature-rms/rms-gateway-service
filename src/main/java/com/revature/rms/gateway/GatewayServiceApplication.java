b575d24ee4991a3bbd07b9358631758cfb449d98
Evan Hsi <evanhsi@Evans-MacBook-Pro.local>
2020-03-04 16:01:49 -0500

    messing around

--- src/main/java/com/revature/rms/gateway/GatewayServiceApplication.java
+++ src/main/java/com/revature/rms/gateway/GatewayServiceApplication.java
@@ -3,7 +3,11 @@ package com.revature.rms.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @EnableEurekaClient
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -12,4 +16,9 @@ public class GatewayServiceApplication {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
+//	@Bean
+//	UiConfiguration uiConfig() {
+//		return new UiConfiguration("validatorUrl", "list", "alpha", "stream",
+//				UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS, false, true, 60000L);
+//	}
 }
