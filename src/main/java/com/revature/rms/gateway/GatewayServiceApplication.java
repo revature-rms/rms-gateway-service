4ce4a90d7f94c45faebb45808e4aa5202fbe2a0e
wezley <wezley.singleton@gmail.com>
2020-10-14 20:37:33 -0400

    another breadcrumb to ensure FARGATE_IP env var is properly set

--- src/main/java/com/revature/rms/gateway/GatewayServiceApplication.java
+++ src/main/java/com/revature/rms/gateway/GatewayServiceApplication.java
@@ -1,5 +1,6 @@
 package com.revature.rms.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -8,7 +9,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 public class GatewayServiceApplication {
 
+	@Value("${FARGATE_IP}")
+	private static String fargateIp;
+
 	public static void main(String[] args) {
+		System.out.println(fargateIp);
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
