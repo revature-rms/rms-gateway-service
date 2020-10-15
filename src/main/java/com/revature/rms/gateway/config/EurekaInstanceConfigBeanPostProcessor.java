da73f4220682e437637829df16bb67f177057c83
wezley <wezley.singleton@gmail.com>
2020-10-14 21:11:25 -0400

    using a bean post processor to set ip address on the eurekaInstanceConfigBean to the ip address of the fargate instance

---
+++ src/main/java/com/revature/rms/gateway/config/EurekaInstanceConfigBeanPostProcessor.java
@@ -0,0 +1,30 @@
+package com.revature.rms.gateway.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EurekaInstanceConfigBeanPostProcessor implements BeanPostProcessor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EurekaInstanceConfigBeanPostProcessor.class);
+
+    @Value("${FARGATE_IP}")
+    private String fargateIp;
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) {
+        LOGGER.info("EurekaInstanceConfigBeanPostProcessor - BeanPostProcessor.postProcessAfterInitialization invoked for bean with name: {}", beanName);
+
+        if (bean instanceof EurekaInstanceConfigBean) {
+            LOGGER.info("EurekaInstanceConfigBean detected. Setting IP address to {}", fargateIp);
+            ((EurekaInstanceConfigBean) bean).setIpAddress(fargateIp);
+        }
+
+        return bean;
+    }
+
+}
